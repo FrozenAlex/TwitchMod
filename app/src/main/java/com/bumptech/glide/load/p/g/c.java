94f123431041413d3e93aab25ed45c7114c5af9a
nopbreak <nopbreak@gmail.com>
2019-10-07 11:25:00 +0300

    Patch: disable animations (gifs in chat)

---
+++ app/src/main/java/com/bumptech/glide/load/p/g/c.java
@@ -0,0 +1,34 @@
+package com.bumptech.glide.load.p.g;
+
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+// Source: GifDrawable
+public class c  extends Drawable {
+    @Override
+    public void draw(@NonNull Canvas canvas) {
+
+    }
+
+    @Override
+    public void setAlpha(int alpha) {
+
+    }
+
+    @Override
+    public void setColorFilter(@Nullable ColorFilter colorFilter) {
+
+    }
+
+    @Override
+    public int getOpacity() {
+        return 0;
+    }
+
+
+    public void start() {
+    }
+}
--- app/src/main/java/tv/twitch/a/m/m/b/p/e.java
+++ app/src/main/java/tv/twitch/a/m/m/b/p/e.java
@@ -1,6 +1,16 @@
 package tv.twitch.a.m.m.b.p;
 
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+
+import com.bumptech.glide.load.p.g.c;
+
+import h.v.d.j;
+
+// Source: UrlDrawable
 public class e {
+    private Drawable a;
+
     public enum b {
         b(24.0f), // Emote
         c(18.0f); // Badge
@@ -13,6 +23,17 @@ public class e {
 
         public final float a() {
             return this.a;
+        }
+    }
+
+    public void draw(Canvas canvas) {
+        j.b(canvas, "canvas");
+        Drawable drawable = this.a;
+        if (drawable != null) {
+            drawable.draw(canvas);
+            if (drawable instanceof c) {
+                // ((c) drawable).start(); // TODO: remove call
+            }
         }
     }
 }
