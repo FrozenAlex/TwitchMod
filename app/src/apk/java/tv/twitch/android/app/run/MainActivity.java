f61f35a1626dd0465c1c0c41050eebfccf4e752d
Roman <nopbreak@gmail.com>
2020-02-08 15:32:48 +0300

    app: fix apk building

--- app/src/apk/java/tv/twitch/android/app/run/MainActivity.java
+++ app/src/apk/java/tv/twitch/android/app/run/MainActivity.java
@@ -3,20 +3,9 @@ package tv.twitch.android.app.run;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.SpannedString;
-import android.util.Log;
 
 import tv.twitch.android.app.R;
 import tv.twitch.android.mod.activities.Settings;
-import tv.twitch.android.mod.bridges.ChatMessageFactory;
-import tv.twitch.android.mod.emotes.EmoteManager;
-import tv.twitch.android.mod.settings.PrefManager;
-import tv.twitch.android.mod.utils.Helper;
-import tv.twitch.android.mod.utils.LoaderLS;
-import tv.twitch.android.mod.utils.Logger;
-import tv.twitch.android.models.channel.ChannelInfo;
-
-import static tv.twitch.android.mod.utils.ChatUtils.injectBadges;
 
 public class MainActivity extends Activity {
 
