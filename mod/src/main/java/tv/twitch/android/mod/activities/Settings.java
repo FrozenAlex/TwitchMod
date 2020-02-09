0537afbc947c29ab1f5fee6cf5abbd09c0355c9f
Roman <nopbreak@gmail.com>
2020-02-09 10:39:24 +0300

    mod: temporary fix for settings crashing

--- mod/src/main/java/tv/twitch/android/mod/activities/Settings.java
+++ mod/src/main/java/tv/twitch/android/mod/activities/Settings.java
@@ -4,11 +4,12 @@ package tv.twitch.android.mod.activities;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
 
-import tv.twitch.android.core.activities.BaseActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
 import tv.twitch.android.mod.utils.Helper;
 
 
-public class Settings extends BaseActivity {
+public class Settings extends AppCompatActivity {
     public static int XML_ID = 0x7f160001;
 
     @Override
