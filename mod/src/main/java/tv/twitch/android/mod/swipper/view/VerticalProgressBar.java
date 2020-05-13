d204678049aedce9e3dd2b3bc05cc289cbd010ac
Roman <nopbreak@gmail.com>
2020-05-13 13:12:45 +0300

    mod: Fix progress shadow size

--- mod/src/main/java/tv/twitch/android/mod/swipper/view/VerticalProgressBar.java
+++ mod/src/main/java/tv/twitch/android/mod/swipper/view/VerticalProgressBar.java
@@ -13,6 +13,7 @@ import android.widget.ProgressBar;
 public class VerticalProgressBar extends ProgressBar {
     private static final int BACKGROUND_COLOR = Color.WHITE;
     private static final int PROGRESS_COLOR = Color.parseColor("#6441A5");
+    private static final int BORDER_WIDTH = 1;
 
     public VerticalProgressBar(Context context) {
         super(context,null, android.R.attr.progressBarStyleHorizontal);
@@ -24,7 +25,7 @@ public class VerticalProgressBar extends ProgressBar {
     private static Drawable generateProgressDrawable() {
         GradientDrawable backgroundShape = new GradientDrawable(GradientDrawable.Orientation.RIGHT_LEFT, new int[]{BACKGROUND_COLOR, BACKGROUND_COLOR, BACKGROUND_COLOR});
         GradientDrawable progressShape = new GradientDrawable(GradientDrawable.Orientation.RIGHT_LEFT, new int[]{PROGRESS_COLOR, PROGRESS_COLOR, PROGRESS_COLOR});
-        backgroundShape.setStroke(1, Color.BLACK);
+        backgroundShape.setStroke(BORDER_WIDTH, Color.BLACK);
 
         ClipDrawable progressClip = new ClipDrawable(progressShape, Gravity.BOTTOM, ClipDrawable.VERTICAL);
         Drawable[] layers = {backgroundShape, progressClip};
